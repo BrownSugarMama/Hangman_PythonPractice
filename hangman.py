b002c231b17fd37b20638b9b1a7966a908099a3c
neechie <neechieg@gmail.com>
2018-02-15 12:27:01 -0500

    code setup: wprds for  game imported

--- hangman.py
+++ hangman.py
@@ -1,3 +1,14 @@
 msg = 'Hello World'
 print(msg)
 
+    # Get a random word : List of words that will be picked for the player
+
+
+import random 
+
+    # Small sample list of words for the game
+words = ['general, assembly, developer, engineer, immersive, fifteen, washington, ']
+
+#lives remaining
+#letters guessed
+
