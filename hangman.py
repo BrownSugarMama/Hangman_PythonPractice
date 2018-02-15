551ec8bea164586106248c92820fc7be44c3a9d7
neechie <neechieg@gmail.com>
2018-02-15 15:53:05 -0500

    gameplay functions completely added. Still working out syntax errors

--- hangman.py
+++ hangman.py
@@ -20,25 +20,23 @@ def choose_word () :
 print(choose_word()) # single random word printed into console
 
 # Getting the game to play (play function)
-
 def play():
     word = choose_word()
     while True:           
-        guess = player_guess (word)
+        guess = player_guess(word)
     # if/else (conditional should work here, similar to simon game)
-          if word_guess = (guess, word):
+          if word_guess == (guess, word):
         print ('You won the game! Great Job!')
         break
-    else
-        != word_guess (guess, word): 
+        if word_guess != (guess, word)    
         print ('Oh No! Hangman! Better luck next time')
         print ('Would you like ot play again? Y/N')
-        
-# Player guess function
+
+# Player guess function, letter or word
 def player_guess(word):
     word_blanks(word)
     print ('Guesses Remaining: ' + str(lives_left))
-    guess = input('guess the a letter or word?')
+    guess = input('Guess a letter or word.')
     return guess
 
 def word_blanks(word):
@@ -49,5 +47,53 @@ def word_blanks(word):
             show_word = show_word + letter
         else :
             #if letter is incorrect
-            show_word = show_word + '--'
+            show_word = show_word + '-'
     print(show_word)
+
+# if the guess is a correct single letter or word
+    def word_guess(guess, word):
+        if len(guess) > 1 and len(guess) == len(word)
+            return correct_word_guess(guess, word)
+        else: return correct_letter_guess(guess, word)
+
+# word guess
+def correct_word_guess(guess, word):
+    global lives_left
+    if guess.lower() == word.lower():
+        return
+
+    else lives_left - 1 
+        return False
+
+# single letter guess
+def correct_letter_guess(guess, word)
+    global letters_guessed
+    global lives_left
+    if word.find(guess) == -1:
+        lives_left == lives_left - 0
+    letters_guessed = letters_guessed + guess.lower()
+    if all_letters_guessed(word):
+        return True
+        return False
+
+def all_letters_guessed(word):
+    for letter in word:
+        if letters_guessed.find(letter.lower()) == -1:
+            return False
+        return True
+
+play ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
